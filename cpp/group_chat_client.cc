0b1ff94a67ec5bf32bcd415be9e2817558ace61f
huankuangwu <hkwu6013@gmail.com>
2023-05-02 13:00:04 -0400

    created client code and Makefile

---
+++ cpp/group_chat_client.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "../build/group_chat.grpc.pb.h"
+
+using grpc::ClientContext;
+using grpc::ClientReaderWriter;
+using grpc::Channel;
+
+using group_chat::GroupChat;
+using group_chat::Message;
+
+
+ABSL_FLAG(std::string, user, "YouKnowWho", "Enter your name.");
+
+class GroupChatClient {
+ public:
+  GroupChatClient(std::shared_ptr<Channel> channel, std::string user)
+    : stub_(GroupChat::NewStub(channel)), username(user){
+  };
+  void Chat(){
+    ClientContext context;
+    std::shared_ptr<ClientReaderWriter<Message, Message>> stream(
+      stub_->StartChat(&context)
+    );
+    std::thread sender_thread(&GroupChatClient::Sender, this, stream);
+    Message msg;
+    while (stream->Read(&msg)){
+      std::cout << "[" << msg.user() << "]: " <<msg.body() << std::endl;
+    }
+    sender_thread.join();
+    return;
+  }
+ private:
+  void Sender(std::shared_ptr<ClientReaderWriter<Message, Message>> stream){
+    std::string input;
+    Message msg;
+    msg.set_user(username);
+    while (true){
+      std::getline(std::cin, input);
+      if (input == "!quit") break;
+      msg.set_body(input);
+      stream->Write(msg);
+    }
+    stream->WritesDone();
+    return;
+  }
+  std::string username;
+  std::unique_ptr<GroupChat::Stub> stub_;
+};
+int main(int argc, char** argv){
+  absl::ParseCommandLine(argc, argv);
+  std::string username = absl::GetFlag(FLAGS_user);
+  std::string server_address("localhost:50051");
+  
+  std::shared_ptr<Channel> channel = grpc::CreateChannel(
+    server_address, grpc::InsecureChannelCredentials());
+
+  // Instantiate the GroupChatClient and start chatting
+  GroupChatClient client(channel, username);
+  client.Chat();
+
+  return 0;
+}
--- cpp/group_chat_server.cc
+++ cpp/group_chat_server.cc
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <mutex>
 #include <vector>
+#include <string>
 
 #include <grpcpp/grpcpp.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
 #include "../build/group_chat.grpc.pb.h"
 
 
 using grpc::Status;
+using grpc::Server;
+using grpc::ServerBuilder;
 using grpc::ServerReaderWriter;
 using grpc::ServerContext;
 using group_chat::GroupChat;
@@ -14,28 +21,56 @@ using group_chat::Message;
 
 class GroupChatImpl final: public GroupChat::Service{
  public: 
-  Status StartChat(ServerContext context,
+  Status StartChat(ServerContext* context,
                    ServerReaderWriter<Message, Message>* stream){
     {
-      // register the new user
+      // register the new user stream
       std::unique_lock<std::mutex> lock(mu_);
       streams.push_back(stream);
+      std::cout << "New user in the room, # users = "
+      << streams.size() << std::endl;
     }
     Message msg;
     
     while (stream->Read(&msg)){
+      std::unique_lock<std::mutex> lock(mu_);
+
+      std::cout << "Received message: " << std::endl << msg.body() 
+      << std::endl << "from " << msg.user()
+      << ". Broadcasting..." << std::endl;
       // broadcast
       for (auto s: streams){
+        if (s == stream) continue;
         s->Write(msg);
       }
     }
     // remove the current stream
-    streams.erase(std::remove(streams.begin(), streams.end(), stream), 
-                  streams.end());
-
+    {
+      std::unique_lock<std::mutex> lock(mu_);
+      streams.erase(std::remove(streams.begin(), streams.end(), stream), 
+                    streams.end());
+    }
+    std::cout << "User left the room, # of users = "
+    << streams.size() << std::endl;
     return Status::OK;
   }
  private:
   std::vector<ServerReaderWriter<Message, Message>*> streams;
   std::mutex mu_;
 };
+void RunServer(){
+  std::string server_address("localhost:50051");
+  ServerBuilder builder;
+  GroupChatImpl group_chat_service;
+  builder.AddListeningPort(
+    server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&group_chat_service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on "<<server_address << std::endl;
+  server->Wait();
+}
+
+int main(){
+  RunServer();
+  return 0;
+}
